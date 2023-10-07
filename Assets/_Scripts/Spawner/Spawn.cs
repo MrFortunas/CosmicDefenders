a86f3a799c3a42b05849fd535ed7348712665660
egipop <egipopo@gmail.com>
2023-10-07 13:52:43 +0300

    Attached scripts in scene

--- Assets/_Scripts/Spawner/Spawn.cs
+++ Assets/_Scripts/Spawner/Spawn.cs
@@ -5,8 +5,10 @@ public class Spawn : MonoBehaviour
 {
     public int count_1 = 0;
     public int count_2 = 0;
+    public int count_3 = 0;
     public Transform enemy1= null;
     public Transform enemy2= null;
+    public Transform enemy3 = null;
     public float timer = 0f;
     private float timerDef = 0f;
     public Vector3 pos;
@@ -32,6 +34,11 @@ public class Spawn : MonoBehaviour
             {
                 Instantiate(enemy2, pos, rot);
                 count_2--;
+            }
+            if (count_2 > 0)
+            {
+                Instantiate(enemy3, pos, rot);
+                count_3--;
             }
         }
     }
